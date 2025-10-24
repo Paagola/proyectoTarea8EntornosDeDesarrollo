ca00a21a9ed0861cf9227b72fbd9ebe6c2488cf1
victor pagola <victorpagola.w@gmail.com>
2025-10-24 13:14:50 +0200

    new: changes by Víctor

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    
+    // Cambio de Víctor
+    System.out.println("Hecho por Víctor º_º");
     }
 }
